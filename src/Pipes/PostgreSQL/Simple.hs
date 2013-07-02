86f4a973d70da74eff11760d37107cedb77a016a
Oliver Charles <ollie@ocharles.org.uk>
2013-07-02 14:56:50 +0100

    Updating to latest dependencies

--- src/Pipes/PostgreSQL/Simple.hs
+++ src/Pipes/PostgreSQL/Simple.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Control.Proxy.PostgreSQL.Simple where
+module Pipes.PostgreSQL.Simple where
 
 import qualified Control.Concurrent.Async as Async
 
@@ -11,24 +11,24 @@ import Data.ByteString (ByteString)
 import Data.Text ()
 import Data.Text.Encoding
 
-import qualified Control.Proxy as Pipes
+import qualified Control.Concurrent.STM as STM
 import qualified Database.PostgreSQL.Simple as Pg
 import qualified Database.PostgreSQL.Simple.Internal as Pg
 import qualified Database.PostgreSQL.LibPQ as LibPQ
-import qualified Control.Proxy.Concurrent as Pipes
-
-import qualified Control.Concurrent.STM as STM
+import qualified Pipes
+import qualified Pipes.Concurrent as Pipes
 
 --------------------------------------------------------------------------------
-query :: (Pg.FromRow r, Pipes.Proxy p, Pg.ToRow params) =>
-    Pg.Connection -> Pg.Query -> params -> () -> Pipes.Producer p r IO ()
-query c q p () = Pipes.runIdentityP $ do
+query
+    :: (Pg.FromRow r, Pg.ToRow params)
+    => Pg.Connection -> Pg.Query -> params -> () -> Pipes.Producer r IO ()
+query c q p () = do
     (i, o) <- lift (Pipes.spawn Pipes.Single)
     worker <- lift $ Async.async $ do
         Pg.fold c q p () (const $ void . STM.atomically . Pipes.send i)
         Pipes.performGC
     lift $ Async.link worker
-    Pipes.recvS o ()
+    Pipes.fromOutput o ()
     lift $ do
         Pipes.performGC
         Async.wait worker
@@ -37,9 +37,9 @@ query c q p () = Pipes.runIdentityP $ do
 
 --------------------------------------------------------------------------------
 -- | Stream a ByteString into the stdin of a COPY statement.
-copyTextIn :: Pipes.Proxy p =>
-    Pg.Connection -> String -> () -> Pipes.Consumer p (Maybe ByteString) IO ()
-copyTextIn c tableName () = Pipes.runIdentityP $ do
+copyTextIn
+    :: Pg.Connection -> String -> () -> Pipes.Consumer (Maybe ByteString) IO ()
+copyTextIn c tableName () = do
     lift $ Pg.withConnection c $ \c' ->
         LibPQ.exec c'
             (encodeUtf8 $ fromString $ "COPY " ++ tableName ++ " FROM STDIN WITH (FORMAT TEXT)")
